0681ab0c9d5367bc579586852862623788077010
Meek Karan <meekkaran@gmail.com>
2023-01-17 11:34:49 +0300

    first commit

---
+++ level1/Easy/runningSum.py
@@ -0,0 +1,10 @@
+#O(n) time | O(n)space
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        output = []
+        total = 0
+        for num in nums:
+            total += num
+            output.append(total)
+        return output
+
