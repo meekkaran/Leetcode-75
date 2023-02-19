7b1b1022dfcbb35b0962dd3ffa220e8108a931a4
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2023-02-19 10:50:58 +0300

    Update BinaryTreeRightSideView.py

--- level2/BinaryTreeRightSideView.py
+++ level2/BinaryTreeRightSideView.py
@@ -12,8 +12,8 @@ class Solution:
             if root:
                 if len(res) == lvl:
                     res.append(root.val)
-                    solve(root.right,lvl+1)
-                    solve(root.left,lvl+1)
+                solve(root.right,lvl+1)
+                solve(root.left,lvl+1)
             return 
 
         res = []
