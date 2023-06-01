53816dc4ad49ccfceac793fa025e66abd78b8c9a
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2023-06-01 11:27:50 +0300

    Update LongestCommonPrefix.py

--- level2/LongestCommonPrefix.py
+++ level2/LongestCommonPrefix.py
@@ -7,4 +7,17 @@ class Solution:
         for i , c in enumerate(str1):
             if c != str2[i]:
                 return str1[:i]
-        return str1
+
+           return str1
+    
+###solution 2
+## O(n(log(n)) time and O(n) space
+        ans = ""
+        v = sorted(strs)
+        v1 = v[0]
+        v2 = v[-1]
+        for i in range(min(len(v1),len(v2))):
+            if v1[i] != v2[i]
+                return ans
+            ans += v1[i]
+        return ans
